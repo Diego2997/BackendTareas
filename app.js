a938a975054cf7149c28c93597cc2b7f1b5b4705
Diego Mercado <diego.mercado1997@hotmail.com>
2023-06-29 20:10:49 -0300

    se creo el modelo Tarea y se realizo la configuracion inicial

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hola")
+import express from "express";
+import cors from 'cors'
+import morgan from 'morgan'
+import path from 'path'
+import 'dotenv/config'
+
+const app = express()
+
+app.set("port",process.env.PORT || 4000)
+
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.static(path.join(__dirname,'/public')))
+
+
+
+app.listen(app.get("port"),()=>{
+    console.log("Servidor corriendo en el puerto ",app.get("port"))
+})
---
+++ src/models/tarea.js
@@ -0,0 +1,18 @@
+import { model,Schema } from "mongoose";
+
+const tareaSchema = new Schema({
+    nombreTarea:{
+    type:String,
+    required:true,
+    minLength:2,
+    maxLength:100,
+    unique:true
+    },
+    completada:{
+    type:Boolean,
+    default:true
+}
+})
+
+const Tarea = model("tarea",tareaSchema)
+export default Tarea
